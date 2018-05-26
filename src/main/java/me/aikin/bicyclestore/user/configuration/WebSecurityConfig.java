635196ccd90355b872164976104e6b5f03b58524
aikin <1@aikin.me>
2018-05-26 17:29:05 +0800

    aikin: optimize import pakcage

--- src/main/java/me/aikin/bicyclestore/user/configuration/WebSecurityConfig.java
+++ src/main/java/me/aikin/bicyclestore/user/configuration/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import me.aikin.bicyclestore.user.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -17,7 +16,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity

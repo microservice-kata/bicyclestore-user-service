8981e3e6698ab464ff0241cefd250bf1f8053c31
aikin <1@aikin.me>
2018-06-28 23:56:44 +0800

    feat(update sql):

--- src/main/resources/db/migration/mysql/MySQL-201805242225__create_user_table.sql
+++ src/main/resources/db/migration/mysql/MySQL-201805242225__create_user_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `USER` (
-    `id`         BIGINT(20)   NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `id`         BIGINT(20)   UNSIGNED NOT NULL AUTO_INCREMENT  COMMENT 'id',
     `name`       VARCHAR(40)  NOT NULL COMMENT '名字',
     `username`   VARCHAR(15)  NOT NULL COMMENT '用户名',
     `email`      VARCHAR(40)  NOT NULL,
     `password`   VARCHAR(255) NOT NULL,
-    `created_at` DATETIME              DEFAULT NULL,
-    `updated_at` DATETIME              DEFAULT NULL,
+    `created_at` TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` TIMESTAMP(3) NULL DEFAULT 0,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_user_username` (`username`),
     UNIQUE KEY `uk_user_email` (`email`)
--- src/main/resources/db/migration/mysql/MySQL-201805242236__create_role_table.sql
+++ src/main/resources/db/migration/mysql/MySQL-201805242236__create_role_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `ROLE` (
-    `id`   BIGINT(20)  NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(60) NOT NULL,
+    `id`   BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(60)         NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_role_name` (`name`)
 )

b5e79a36d61fc4efd21d4611ae0d7011b85d7934
aikin <1@aikin.me>
2018-08-26 21:55:12 +0800

    feat(MIGRATION): fix migrate

--- src/main/resources/db/migration/mysql/MySQL-201805242240__create_user_role_table.sql
+++ src/main/resources/db/migration/mysql/MySQL-201805242240__create_user_role_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `USER_ROLE` (
-    `user_id` BIGINT(20) NOT NULL,
-    `role_id` BIGINT(20) NOT NULL,
+    `user_id` BIGINT(20) UNSIGNED NOT NULL,
+    `role_id` BIGINT(20) UNSIGNED NOT NULL,
     PRIMARY KEY (`user_id`, `role_id`),
-    KEY `fk_user_roles_role_id` (`role_id`),
+    KEY `fk_user_role_role_id` (`role_id`),
     CONSTRAINT `fk_user_role_role_id` FOREIGN KEY (`role_id`) REFERENCES `ROLE` (`id`),
     CONSTRAINT `fk_user_role_user_id` FOREIGN KEY (`user_id`) REFERENCES `USER` (`id`)
 )
